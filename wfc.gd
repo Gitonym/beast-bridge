e0c6ca74e3fa0c98495aebb752fa78aaf29a1913
Florim Sabani <florim.sabani@mercedes-benz.com>
2024-09-11 15:42:04 +0200

    added some profiling

--- wfc.gd
+++ wfc.gd
@@ -74,17 +74,19 @@ func _ready():
 			Vector3.FORWARD: [&"ground", &"air"]
 		}).track().generate_rotations()
 	
-	var start = Time.get_ticks_usec()
+	var time1 = Time.get_ticks_usec()
 	
 	wfc = WaveFunctionCollapseGrid.new(20, 5, 20, 4, CellItem.definitions)
 	add_child(wfc)
 	wfc.collapse_all()
 	
-	var end = Time.get_ticks_usec()
-	print("Execution time:")
-	print((end-start)/1000000.0, " μs")
+	var time2 = Time.get_ticks_usec()
+	print("Time to collapse: ", (time2-time1)/1000000.0, " μs")
 	
 	wfc.spawn_items()
+	
+	var time3 = Time.get_ticks_usec()
+	print("Time to spawn: ", (time3-time2)/1000000.0, " μs")
 	
 	print(wfc.count_rotations(&"ramp"))
 	print(wfc.count_rotations(&"gate"))
